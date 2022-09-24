0b248241ce43d518a1bd2f55bf10f9a4f8429947
KrcDigital <krc@krccorp.net>
2022-09-24 23:54:58 +0300

    -KRC-

---
+++ app/src/main/java/com/krc/findup/like.kt
@@ -0,0 +1,15 @@
+package com.krc.findup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class like : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_like)
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/krc/findup/messagee.kt
@@ -0,0 +1,11 @@
+package com.krc.findup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class messagee : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messagee)
+    }
+}
